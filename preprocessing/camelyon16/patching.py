4e7d0226731ebd04c3953d930fb7e32088c61e94
Mai Anh Vu <maianhuel@gmail.com>
2025-05-04 17:20:03 -0500

    update

--- preprocessing/camelyon16/patching.py
+++ preprocessing/camelyon16/patching.py
@@ -220,7 +220,7 @@ def extract_and_save_patches(
 def main():
     dataset_name = "camelyon16" 
     parser = argparse.ArgumentParser(description="Extract patches from WSI TIFFs with XML mask filtering")
-    parser.add_argument("--config", type=str, default=f"config/data_{dataset_name}.yaml", help="Path to YAML config file")
+    parser.add_argument("--config", type=str, default=f"configs/data_{dataset_name}.yaml", help="Path to YAML config file")
     parser.add_argument("--patch_size", type=int, default=256, help="Size of each patch")
     parser.add_argument("--stride", type=int, default=256, help="Stride for patch extraction")
     parser.add_argument("--tissue_threshold", type=float, default=0.1, help="Minimum tissue fraction to keep patch")
