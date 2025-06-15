e69ef7ae88c1529b0cc86c345a3e3b823b337872
Mai Anh Vu <maianhuel@gmail.com>
2025-06-15 18:02:33 -0500

    update

--- src/datasets/multiple_scales/tcga.py
+++ src/datasets/multiple_scales/tcga.py
@@ -5,13 +5,6 @@ import h5py
 from torch.utils.data import Dataset
 import numpy as np
 
-import os
-import pandas as pd
-import torch
-import h5py
-from torch.utils.data import Dataset
-import numpy as np
-
 class Generic_MIL_Dataset(Dataset):
     def __init__(self,
                  data_dir_s,
@@ -45,63 +38,52 @@ class Generic_MIL_Dataset(Dataset):
     def __len__(self):
         return len(self.slide_data)
 
-    def _resolve_subtype_path(self, slide_id, path_dict):
-        # Better: use patient_id if available
-        subtype = None
+    def _resolve_subtype_path(self, slide_id, path_dict, patient_id=None):
+        # Try matching with subtype via patient_id if available
         for key in path_dict:
             if key.lower() in slide_id.lower():
-                subtype = key
-                break
-        if subtype is None and 'patient_id' in self.slide_data.columns:
-            row = self.slide_data[self.slide_data['slide_id'] == slide_id]
-            pid = row['patient_id'].values[0]
-            for key in path_dict:
-                if key.lower() in pid.lower():
-                    subtype = key
-                    break
-        if subtype is None:
-            raise ValueError(f"Cannot match slide_id '{slide_id}' to any subtype in {list(path_dict.keys())}")
-        return path_dict[subtype]
+                return path_dict[key]
+            if patient_id and key.lower() in patient_id.lower():
+                return path_dict[key]
+        raise ValueError(f"Cannot match slide_id '{slide_id}' or patient_id '{patient_id}' to any subtype in {list(path_dict.keys())}")
 
     def __getitem__(self, idx):
         row = self.slide_data.iloc[idx]
         slide_id = row['slide_id']
         label_str = row['label']
         label = self.label_dict[label_str]
+        patient_id = row['patient_id']
 
-        folder_s = self._resolve_subtype_path(slide_id, self.data_dir_s)
-        folder_l = self._resolve_subtype_path(slide_id, self.data_dir_l)
+        folder_s = self._resolve_subtype_path(slide_id, self.data_dir_s, patient_id)
+        folder_l = self._resolve_subtype_path(slide_id, self.data_dir_l, patient_id)
 
-        if not self.use_h5:
-            pt_path_s = os.path.join(folder_s, f"{slide_id}.h5")
-            pt_path_l = os.path.join(folder_l, f"{slide_id}.h5")
-            features_s = torch.load(pt_path_s, weights_only=True)
-            features_l = torch.load(pt_path_l, weights_only=True)
-            
-            return features_s, None, features_l, None, label
-        else:
-            h5_path_s = os.path.join(folder_s, 'h5_files', f"{slide_id}.h5")
-            h5_path_l = os.path.join(folder_l, 'h5_files', f"{slide_id}.h5")
+        if self.use_h5:
+            h5_path_s = os.path.join(folder_s, f"{slide_id}.h5")
+            h5_path_l = os.path.join(folder_l, f"{slide_id}.h5")
 
             try:
                 with h5py.File(h5_path_s, 'r') as f_s:
                     features_s = torch.from_numpy(f_s['features'][:])
                     coords_s = torch.from_numpy(f_s['coords'][:])
             except Exception:
-                pt_path_s = os.path.join(folder_s, f"{slide_id}.pt")
-                features_s = torch.load(pt_path_s, weights_only=True)
-                coords_s = None
+                features_s, coords_s = None, None
 
             try:
                 with h5py.File(h5_path_l, 'r') as f_l:
                     features_l = torch.from_numpy(f_l['features'][:])
                     coords_l = torch.from_numpy(f_l['coords'][:])
             except Exception:
-                pt_path_l = os.path.join(folder_l, f"{slide_id}.pt")
-                features_l = torch.load(pt_path_l, weights_only=True)
-                coords_l = None
+                features_l, coords_l = None, None
 
             return features_s, coords_s, features_l, coords_l, label
+
+        else:
+            pt_path_s = os.path.join(folder_s, f"{slide_id}.pt")
+            pt_path_l = os.path.join(folder_l, f"{slide_id}.pt")
+            features_s = torch.load(pt_path_s, map_location='cpu')
+            features_l = torch.load(pt_path_l, map_location='cpu')
+            return features_s, None, features_l, None, label
+
 
 def return_splits_custom(
     train_csv_path,
