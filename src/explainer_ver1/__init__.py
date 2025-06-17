d940a6e8139a98b0f594fa525ce1bd30dde49a53
Mai Anh Vu <maianhuel@gmail.com>
2025-06-17 14:19:29 -0500

    update

--- src/explainer_ver1/__init__.py
+++ src/explainer_ver1/__init__.py
@@ -3,3 +3,4 @@ from .prompt_learner import PromptLearner
 from .text_encoder import TextEncoder 
 from .model_utils import MultiheadAttention
 from .mil_ver1 import ExplainerVer1
+from .mil_ver1b import ViLa_MIL_Model
