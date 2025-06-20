04e70ef8503c3b48611855cbb837ac8eca7d3132
Mai Anh Vu <maianhuel@gmail.com>
2025-06-20 08:16:58 -0500

    update

--- check_conch_vlm.py
+++ check_conch_vlm.py
@@ -8,7 +8,7 @@ from PIL import Image
 conch_path = os.path.abspath("src/externals/CONCH")  # adjust if needed
 sys.path.append(conch_path)
 
-from conch.open_clip_custom import create_model_from_pretrained, tokenize
+from conch.open_clip_custom import create_model_from_pretrained, tokenize, get_tokenizer
 
 # === HF cache and device ===
 os.environ['HF_HOME'] = '/project/hnguyen2/mvu9/folder_04_ma/cache_folder/.cache/huggingface'
@@ -30,22 +30,20 @@ random_array = np.random.randint(0, 256, (224, 224, 3), dtype=np.uint8)
 image = Image.fromarray(random_array)
 image_tensor = preprocess(image).unsqueeze(0).to(device)  # [1, 3, 224, 224]
 
-# === Create dummy text prompts ===
-prompts = ["A WSI of tumor", "A WSI of normal tissue"]
-tokens = tokenize(prompts).to(device)
+# === Inputs ===
+classes = ['invasive ductal carcinoma', 'invasive lobular carcinoma']
+prompts = [f"an H&E image of {cls}" for cls in classes]
 
-# === Encode image and text ===
-with torch.no_grad():
-    image_feat = model.encode_image(image_tensor, proj_contrast=True, normalize=True)  # [1, D]
-    text_feat = model.encode_text(tokens)  # [2, D]
+# === Load tokenizer and tokenize
+tokenizer = get_tokenizer()
+tokenized_prompts = tokenize(texts=prompts, tokenizer=tokenizer).to(device)
 
-    image_feat = image_feat / image_feat.norm(dim=-1, keepdim=True)
-    text_feat = text_feat / text_feat.norm(dim=-1, keepdim=True)
+# === Encode
+with torch.inference_mode():
+    image_embedings = model.encode_image(image_tensor, proj_contrast=True, normalize=True)
+    text_embedings = model.encode_text(tokenized_prompts, proj_contrast=True, normalize=True)
+    sim_scores = (image_embedings @ text_embedings.T * model.logit_scale.exp()).softmax(dim=-1).cpu().numpy()
 
-    similarity = image_feat @ text_feat.T  # [1, 2]
-    pred = similarity.argmax(dim=1).item()
-
-# === Output ===
-print("Dummy image vs prompts similarity:", similarity.cpu().numpy())
-print("Predicted class index:", pred)
-print("Predicted prompt:", prompts[pred])
+# === Output
+print("Predicted class:", classes[sim_scores.argmax()])
+print("Normalized similarity scores:", [f"{cls}: {score:.3f}" for cls, score in zip(classes, sim_scores[0])]) 
