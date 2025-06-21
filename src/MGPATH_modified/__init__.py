7c331c53ad70f59cd35a6a301d51d6cb2ffd6d43
Mai Anh Vu <maianhuel@gmail.com>
2025-06-21 17:17:06 -0500

    update

--- src/MGPATH_modified/__init__.py
+++ src/MGPATH_modified/__init__.py
@@ -5,7 +5,7 @@
 @update : 2025-Feb-25
 """
 from .adapter import Adapter
-from .plip_projector import PLIPProjector
+# from .plip_projector import PLIPProjector
 from .plip_text_encoder import PLIPTextEncoder
 from .prompt_learner import PromptLearner
 from .model_CONCH_PLIP_adapter_GAT import CONCH_PLIP_adapter_GAT
