a6bb1cd77c877811b14e8e9777ccd699549d1cff
Mai Anh Vu <maianhuel@gmail.com>
2025-06-21 17:29:17 -0500

    update

--- src/MGPATH_modified/model_CONCH_PLIP_adapter_GAT.py
+++ src/MGPATH_modified/model_CONCH_PLIP_adapter_GAT.py
@@ -15,8 +15,8 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, "../"))
 
-import PLIPTextEncoder, PromptLearner 
-import Adapter 
+from MGPATH_modified import PLIPTextEncoder, PromptLearner 
+from MGPATH_modified import Adapter 
 
 
 
