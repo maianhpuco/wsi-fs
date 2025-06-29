4c77ae6cf57c1b6799f5e12994d86a7507f9b5e2
Mai Anh Vu <maianhuel@gmail.com>
2025-06-29 11:49:44 -0500

    update

--- src/explainer_ver2/__init__.py
+++ src/explainer_ver2/__init__.py
@@ -4,4 +4,4 @@ from .text_encoder import TextEncoder
 from .model_utils import MultiheadAttention
 
 from .ver2a_conch_desc_attn_1 import Ver2a
-from .ver2a_conch_desc_attn_2 import Ver2b
+from .ver2b_conch_desc_attn_2 import Ver2b
